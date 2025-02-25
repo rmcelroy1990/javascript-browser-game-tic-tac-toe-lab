f696457c37438bc08b2a57842cc68105872a7560
rmcelroy1990 <mcelroy.raeme@gmail.com>
2025-02-25 01:59:40 -0800

    completed lab

--- js/app.js
+++ js/app.js
@@ -1,132 +1,122 @@
-//const players = {
-   // '1 : 'X',
-   // '-1' : 'O'
-   // 'null' : ''
+    const COMBOS = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ]
 
-    //const COMBOS = [
-       // [0, 1, 2],
-      //  [3, 4, 5],
-      //  [6, 7, 8],
-      //  [0, 3, 6],
-     //   [1, 4, 7],
-     //   [2, 5, 8],
-      //  [0, 4, 8],
-     //   [2, 4, 6],
-  //  ]
-//}
 
 /*---------------------------- Variables (state) ----------------------------*/
 
-let board = ['[0]', '[1]', '[2]', '[3]', '[4]', '[5]', '[6]', '[7]', '[8]',
+let board = ['X', 'X', 'O', 'O', 'O', 'O', 'O', 'O', 'X', 'X'
 ]; //to represent the state of the squares on the board
 let turn = ['X']; //to track whose turn it is
 let winner = ['false']; //to represent if anyone has won yet
-let tie = []; //to represent if the game has ended in a tie
+let tie = ['false']; //to represent if the game has ended in a tie
 let playerO = ['O'];
+
 /*------------------------ Cached Element References ------------------------*/
 
-const squareEls = document.querySelectorAll('.squares');
+const squareEls = document.querySelectorAll('.sqr');
 const messageEl = document.querySelector('h2');
+const resetBtnEl = document.querySelector('click');
 
 //console.log(squareEls)
 
 /*-------------------------------- Functions --------------------------------*/
-function init() {
-}
-//render('game-started');
-//console.log('game-started');
+init();
 
-//function render() {;
+const init = () => {
 
-function updateBoard() {
-    
-//board = [
-       // 'null', 'null', 'null',
-       // 'null', 'null', 'null',
-       // 'null', 'null', 'null'
-//];
-         for(let i = 0; i < squareEls.length; i++); {
-         }     
-     updatedBoard.forEach((squareEls) => {
-        // console.log.push(squareEls[i]);
-     });
-    }
-function updateMessage() {
-    render ()
-    const updateMessage = document.getElementById("update-message");
-   // updateMessage.textContent = "If both winner and tie have a value of false (meaning the game is still in progress), render whose turn it is.
-// If winner is false, but tie is true, render a tie message.
-// Otherwise, render a congratulatory message to the player that has won.";
-}
-  
-function handleClick() {
-
+    console.log(init);
+    render();
 }
 
-//const squareEls = document.querySelector('button').addEventListener('click', handleClick);
+//window.onload = init() {
+    console.log(init);
+//};
 
-function placePiece()
-board.forEach(place, index) => {
-    squareEls[index].textContent = PLAYERS[value]
+const render = () => {
+updateBoard();
+updateMessage();
+};
+
+const updateBoard = () => {
+    for(let i = 0; i < squareEls.length; i++) {
+        board = [
+        'null', 'null', 'null',
+        'null', 'null', 'null',
+        'null', 'null', 'null',
+        ]
+    board.forEach(squareEls => {
+    console.log(squareEls);
+});
 }
-//console.log(placePiece);
+}
 
-function checkForWinner() {
-    for(let i = 0; i < winningCombos.length; i++) {
-        if(Math.abs(board[winningCombos[i][0] + board[winningCombos[i][1]]+ board[winningCombos[i][2]]]) === 3 {
-            return turn
+const updateMessage = () => {
+    if (!winner && !tie) {
+        if (turn === 'X') {
+            messageEl.textContent = "It's X's turn";
+        } else {
+            messageEl.textContent = "It's O's turn";
         }
+    } else if (!winner && tie) {
+        messageEl.textContent = "Tie game!";
+    } else { 
+        if ( turn === 'X') {
+          messageEl.textContent = 'X wins!';
+        } else {
+          messageEl.textContent = 'O wins!';
+        }
+      }
+  
+  const handleClick = (evt) => {
+    const sqIdx = evt.target.id;
+    const squareIsFull = board[sqIdx] !== '';
+    if (squareIsFull || winner) {
+      return;
     }
 }
-function checkForTie() {
-   for(let i = 0; i < winningCombos.length; i++) 
-}
+ squareEls.forEach(square => {
+    squareEls.querySelectorAll('click', function(handleClick) {
 
-function switchPlayerTurn() {
-    for(let i = 0; i < winningCombos.length; i++)
-        if(winner === true)
-            return 'X'; {
-} else if (winner === 'T') messageEl.textContent = '$'
-}
-
-render() {
-clear the board game and reset to beginning
-}
-/*----------------------------- Event Listeners -----------------------------*/
-
-document.querySelector('button').addEventListener('click', init);
-//document.getElementById('game-board').addEventListener('click'handleMove)
-
-
-/*-------------------------------- Constants --------------------------------*/
-
-const winningCombos = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ]
+    })
+ })
   
-squareIndex = 
-/*---------------------------- Variables (state) ----------------------------*/
+const squareIndex = board.indexOf();
 
+const placePiece = (idx);
+    board[idx] = turn;
+};
 
+const checkForWinner = () => {
+    for( let i = 0; i < COMBOS.length; i++) {
+         if(Math.abs(board[COMBOS[i][0]] + board[COMBOS[i][1]] + board[COMBOS[i][2]]) === 3) {
+}
+winner = true;
+    }
+}
 
+const checkForTie = () => {
+    if (winner) {
+        return;
+    }
+    if (!board.includes('')) {
+        tie = true;
+    }
+};
 
-/*------------------------ Cached Element References ------------------------*/
-
-
-
-/*-------------------------------- Functions --------------------------------*/
-
-
-
-/*----------------------------- Event Listeners -----------------------------*/
-//document.querySelector('button').addEventListener('click', init);
-//document.getElementById('game-board').addEventListener('click'handleMove)
-
-
+const switchPlayerTurn = () => {
+    if (winner) {
+        return;
+    }
+    if (turn === 'X') {
+        turn = 'O';
+    } else {
+        turn = 'X';
+    }};
